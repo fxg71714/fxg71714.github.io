5117ae9e1291728aa4458dd1cb0c1e11a24a120a
fxg71714 <fxg71714@hawkmail.hacc.edu>
2026-03-16 20:07:41 -0400

    astro problem and exercise 7

--- art114/exercises/exercise-6/astro.config.mjs
+++ art114/exercises/exercise-6/astro.config.mjs
@@ -1,5 +1,9 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  base: '/art114/exercises/exercise-6',
+  build: {
+    site: 'http://yoursite.com//art114/exercises/exercise-6/',
+  }
+});
