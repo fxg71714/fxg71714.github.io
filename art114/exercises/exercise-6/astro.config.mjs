acf797b479c15a9048480d82e4c37d4609c6352e
fxg71714 <fxg71714@hawkmail.hacc.edu>
2026-04-26 21:07:54 -0400

    Delete art114/exercises/exercise-6 directory

--- art114/exercises/exercise-6/astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-export default defineConfig({
-  base: '/art114/exercises/exercise-6',
-  build: {
-    site: 'http://yoursite.com//art114/exercises/exercise-6/',
-  }
-});
